db5e28fc16a24ad617482fe2736dbbd17b3bf393
BSA <email@email.com>
2021-01-17 19:11:27 +0300

    Подправил web.xml, для того что бы заработал EL JSP, а так же добавлена зависимость на JSTL

--- src/main/java/sber/ServletChat.java
+++ src/main/java/sber/ServletChat.java
@@ -9,8 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServletChat extends HttpServlet{
+    private List<String> chatMessages = new ArrayList<>();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Cookie[] cookies = req.getCookies();
@@ -36,10 +40,13 @@ public class ServletChat extends HttpServlet{
         }
         else{
             //отображаем все сообщения чата
-            String path = "/index.jsp";
-            ServletContext servletContext = getServletContext();
-            RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
-            requestDispatcher.forward(req, resp);
+            //передаём список со всеми сообщениями чата на JSP страницу
+            chatMessages.add("Message 1");
+            chatMessages.add("Message 2");
+            chatMessages.add("Message 3");
+
+            req.setAttribute("chatMessages", chatMessages);
+            getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
 
             System.out.println("ServletChat - doGet -"+cookie.getValue());
         }
@@ -61,6 +68,13 @@ public class ServletChat extends HttpServlet{
             //отображаем все сообщения чата
             String path = "/index.jsp";
             ServletContext servletContext = getServletContext();
+
+            //chatMessages.add("Message form Pasha");
+            //chatMessages.add("Message from Vova");
+
+            //передаём список со всеми сообщениями чата на JSP страницу
+            //request.setAttribute("chatMessages",chatMessages);
+            //servletContext.getRequestDispatcher("/index.jsp").forward(request,response);
             RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
             requestDispatcher.forward(request, response);
         }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,10 +1,15 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
 <html>
+<head>
+<meta charset="UTF-8">
+<title>JSP Application</title>
+</head>
 <body>
-<h2>Hello World!</h2>
-<%
-            for(int i = 1; i < 5; i++){
-                out.println("<br> Message - " +i);
-            }
-%>
+        <ul>
+            <c:forEach var="msg" items="${chatMessages}">
+                <li><c:out value="${msg}" /></li>
+            </c:forEach>
+        </ul>
 </body>
 </html>
