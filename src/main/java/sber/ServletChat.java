b38e8b0ce702c22ba64e5c8aa9e8b21239fc705b
BSA <email@email.com>
2021-01-17 15:40:34 +0300

    Реализована авторизация в чате через cookie

--- src/main/java/sber/ServletChat.java
+++ src/main/java/sber/ServletChat.java
@@ -1,6 +1,9 @@
 package sber;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,9 +13,56 @@ import java.io.PrintWriter;
 public class ServletChat extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.write("Servlet Chat!!");
-        writer.close();
-        System.out.println("ServletChat - doGet");
+        Cookie[] cookies = req.getCookies();
+        String cookieName = "login";
+        Cookie cookie = null;
+        if(cookies !=null) {
+            for(Cookie c: cookies) {
+                if(cookieName.equals(c.getName())) {
+                    cookie = c;
+                    break;
+                }
+            }
+        }
+
+        //проверяем что пользователь уже авторизовался
+        if(cookie==null)
+        {
+            //перенаправляем клиента на форму авторизации
+            String path = "/authorization-form.html";
+            ServletContext servletContext = getServletContext();
+            RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+            requestDispatcher.forward(req, resp);
+        }
+        else{
+            //отображаем все сообщения чата
+            String path = "/index.jsp";
+            ServletContext servletContext = getServletContext();
+            RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+            requestDispatcher.forward(req, resp);
+
+            System.out.println("ServletChat - doGet -"+cookie.getValue());
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        String login = request.getParameter("login");
+
+        if(login!=null) {
+            System.out.println("Connect to chat user - " + login);
+            response.addCookie(new Cookie("login", login));
+
+            //отображаем все сообщения чата
+            String path = "/index.jsp";
+            ServletContext servletContext = getServletContext();
+            RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+            requestDispatcher.forward(request, response);
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<%
+            for(int i = 1; i < 5; i++){
+                out.println("<br> Message - " +i);
+            }
+%>
 </body>
 </html>
